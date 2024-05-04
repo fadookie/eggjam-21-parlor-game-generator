c0fe8d5b28d05b8315a2379fe5c103da994bc39b
Eliot Lash <fadookie@gmail.com>
2024-05-04 12:12:49 -0700

    First pass good-enough rules madlib generator

--- generator.ts
+++ generator.ts
@@ -1,27 +1,69 @@
-interface BaseRule {
+interface Rule {
   template: string;
 }
 
-type InitialRuleType = 'Guess';
-
-interface InitialRule extends BaseRule {
-  type: InitialRuleType;
-}
-
-const initialRules: InitialRule[] = [
+const initialRules: Rule[] = [
   {
-    type: 'Guess',
-    template: 'Each player must ${playerVerb1} a ${playerNoun1}.',
+    template: 'In each round, one player must ${playerVerb1} a ${playerNoun1}.',
+  },
+];
+
+const intermediateRules1: Rule[] = [
+  {
+    template: 'Every other player takes turns ${playerVerb2} the ${playerNoun1} based on the original ${playerVerb1AsNoun}.',
+  }
+];
+
+const intermediateRules2: Rule[] = [
+  {
+    template: 'The original player is blindfolded.',
+  },
+  {
+    template: 'The order of the ${playerVerb2AsNoun} are shuffled.',
+  },
+  {
+    template: 'One player is secretly selected as the traitor by drawing a special card, and must try to make all the other players lose.',
+  },
+];
+
+const intermediateRules3: Rule[] = [
+  {
+    template: 'The original player must exactly reproduce the ${playerVerb2AsNoun} of each other player.',
+  },
+  {
+    template: 'Each other player must try to reproduce the ${playerVerb2AsNoun} of each other player in a ring.',
+  },
+];
+
+const finalRules: Rule[] = [
+  {
+    template: 'Everyone judges this, if a majority of players think the original player deserves to win, they win. Otherwise, the other players win.',
+  },
+  {
+    template: 'If a player makes an obvious mistake and a majority of players boo them, they are out of the game. The last player remaining wins.',
   },
 ];
 
 const playerVerb1Options = [
-  'draw',
-  'sing',
-  'mime',
-  'hum',
-  'describe without saying the name of',
-  'rhyme'
+  ['draw', 'drawing'],
+  ['sing', 'song'],
+  ['mime', 'mime performance'],
+  ['hum', 'hummed tune'],
+  ['describe without saying the name of', 'description'],
+  ['rhyme', 'rhyme'],
+];
+
+const playerVerb2Options = [
+  ['guessing', 'guess'],
+  ['singing', 'song'],
+  ['miming', 'mime performance'],
+  ['humming', 'hummed tune'],
+  ['describing without saying the name of', 'description'],
+  ['rhyming', 'rhyme'],
+  ['drawing', 'drawing'],
+  ['interpretive dancing', 'interpretive dance'],
+  ['beatboxing', 'beatbox performance'],
+  ['rapping', 'rap'],
 ];
 
 const playerNoun1Options = [
@@ -46,12 +88,49 @@ function pickRandom<T>(array: T[]): T {
 }
 
 function generateRules() {
+  // Bind variables
+  const playerVerb1Tenses = pickRandom(playerVerb1Options);
+  const playerVerb1 = playerVerb1Tenses[0];
+
+  const playerVerb1AsNoun = playerVerb1Tenses[1];
+  const playerVerb2Tenses = pickRandom(playerVerb2Options);
+  const playerVerb2 = playerVerb2Tenses[0];
+  const playerVerb2AsNoun = playerVerb2Tenses[1];
+
+  const playerNoun1 = pickRandom(playerNoun1Options);
+
+  const allVariables = { playerVerb1, playerVerb1AsNoun, playerVerb2, playerVerb2AsNoun, playerNoun1 };
+
+  // Pick and interpolate rules
   const initialRule = pickRandom(initialRules);
   const initialRuleTemplate = _.template(initialRule.template);
-  const playerVerb1 = pickRandom(playerVerb1Options);
-  const playerNoun1 = pickRandom(playerNoun1Options);
-  const initialRuleText = initialRuleTemplate({ playerVerb1, playerNoun1 });
-  return initialRuleText;
+  const initialRuleText = initialRuleTemplate(allVariables);
+
+  const intermediateRule1 = pickRandom(intermediateRules1);
+  const intermediateRule1Template = _.template(intermediateRule1.template);
+  const intermediateRule1Text = intermediateRule1Template(allVariables);
+
+  const intermediateRule2 = pickRandom(intermediateRules2);
+  const intermediateRule2Template = _.template(intermediateRule2.template);
+  const intermediateRule2Text = intermediateRule2Template(allVariables);
+
+  const intermediateRule3 = pickRandom(intermediateRules3);
+  const intermediateRule3Template = _.template(intermediateRule3.template);
+  const intermediateRule3Text = intermediateRule3Template(allVariables);
+
+  const finalRule = pickRandom(finalRules);
+  const finalRuleTemplate = _.template(finalRule.template);
+  const finalRuleText = finalRuleTemplate(allVariables);
+
+  return `${initialRuleText}
+
+${intermediateRule1Text}
+
+${intermediateRule2Text}
+
+${intermediateRule3Text}
+
+${finalRuleText}`;
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
